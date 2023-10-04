d41513caae6787b26e147ab85c47c4b25c090995
Nguyen Truong Giang <giang.nt.2309@aptechlearning.edu.vn>
2023-10-04 22:13:24 +0700

    giang-footer  add footer

--- frontend/component/Footer/FooterComponent.php
+++ frontend/component/Footer/FooterComponent.php
@@ -2,6 +2,64 @@
 function FooterComponent() {
     ?>
     <link rel="stylesheet" href="../component/Footer/FooterComponent.css"/>
-    <h1 class="footer_h1">Hello world</h1>
+    <div class= "container">
+         <div class= "row">
+            <div class ="col ">
+                <ul class="box-local-nct">
+                    <div class="title_box"><h5>Hỗ trợ</h5></div>
+                    <li><a href="" rel="nofollow" target="_blank" >Trợ Giúp</a></li>
+                    <li><a href="" rel="nofollow" >Sơ Đồ</a></li>
+                    <li><a href="" rel="nofollow" target="_blank">NCCI</a></li>
+                    <li><a href="" rel="nofollow" target="_blank" >Liên Hệ Quảng Cáo</a></li>
+                    <li><a href="" rel="nofollow" >Chính sách Bảo Mật</a></li>
+                    <li><a href="" rel="nofollow" target="_blank" >Chính Sách SHTT</a></li>
+                    <li><a href="" rel="nofollow"> Thoả Thận Sử Dụng</a></li>
+                </ul>
+            </div>
+            <div class ="col">
+                <ul class="box-apps-nct">
+                    <div class="title_box"><h5>Sản Phẩm Khác</h5></div>
+                    <li><a href="" rel="nofollow" target="_blank" >Mobile App</a></li>
+                    <li><a href="" rel="nofollow" target="_blank" >Mobile web</a></li>
+                    <li><a href="" rel="nofollow" target="_blank" >Smart TV</a></li>
+                    <li><a href="" rel="nofollow" target="_blank" >Desktop</a></li>
+                    <li><a href="" rel="nofollow" target="_blank" >Dịch Vụ 3G</a></li>
+                    <li><a href="" rel="nofollow" target="_blank" >NCC Corp</a></li>
+                    <li><a href="" rel="nofollow" target="_blank" >Tuyển Dụng</a></li>
+                    
+                </ul>
+            </div class= title>
+            <div class ="col ">
+                <h5> Kết nối với chúng tôi</h5>
+                       <div><a href=""><i class="fa-brands fa-facebook"></i>Facebook</a></div> 
+                        <div><a href=""><i class="fa-brands fa-tiktok"></i>Tiktok</a></div>
+                        <div><a href=""><i class="fa-brands fa-google"></i>Google</a></div>
+            </div>
+         </div>
+
+    
+       
+            <div class="content-wrap">
+                <div class="address">
+                    <div class="nctcorp_logo"><a href="http://www.nhaccuatui.com/" title="NhacCuaTui.com"></a></div>
+                    <p><strong> Công ty Cổ phần N C T </strong></p>
+                    <ul>
+                        <li>Giấy phép MXH số 499/GP-BTTTT do Bộ Thông Tin và Truyền thông cấp ngày 28/09/2015.</li>
+                        <li>Giấy Chứng nhận Đăng ký Kinh doanh số 0305535715 do Sở kế hoạch và Đầu tư thành phố Hồ Chí Minh cấp ngày 01/03/2008</li>
+                    </ul>
+                </div>
+                <div class="t-separation-line"></div>
+                    
+            </div>
+            <div class="nctcorp_logo">
+                    <h1>NCT</h1>
+                </div>
+            <div class="logo">
+                    <p>CORPORATION</p>
+                </div>
+    </div>
+
+      
     <?php
+
 }
--- frontend/page/index.php
+++ frontend/page/index.php
@@ -1,5 +1,7 @@
 <?php
-include "../../backend/api/User/UserView.class.php";
+//ini_set('display_errors',1);
+//error_reporting(E_ALL);
+//include '../../backend/api/User/UserView.class.php';
 include "../component/Footer/FooterComponent.php";
 ?>
 <!DOCTYPE html>
@@ -8,13 +10,19 @@ include "../component/Footer/FooterComponent.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <title>Homepage</title>
 </head>
 
 <body>
-<?php 
+    <?php
     FooterComponent();
-?>
+    ?>
+
 </body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/e928a09ca2.js" crossorigin="anonymous"></script>
 
 </html>
