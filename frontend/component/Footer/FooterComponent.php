f3a023550e86f06486cf279c93ee0afa80d43f38
Net <tuanhm@tigren.com>
2023-10-05 12:44:27 +0700

    giang-footer solve conflict

--- frontend/component/Footer/FooterComponent.php
+++ frontend/component/Footer/FooterComponent.php
@@ -1,77 +1,71 @@
 <?php
-function FooterComponent() {
+function FooterComponent()
+{
     ?>
-    <link rel="stylesheet" href="../component/Footer/FooterComponent.css"/>
+    <link rel="stylesheet" href="../component/Footer/FooterComponent.css" />
     <footer>
-    <div class="nct-footer">
-    <div class="title"> <h6>Liên kết và hợp tác</h3></div>
-        <div  class="d-flex p-2   justify-content-center" >
-            <div class="">
-                <img src="../asset/img/universal.png" alt="">
+        <div class="nct-footer">
+            <div class="title"><h3>Liên kết và hợp tác</h3></div>
+            <div class="d-flex p-2   justify-content-center">
+                <div class="">
+                    <img src="../asset/img/universal.png" alt="">
+                </div>
+                <div>
+                    <img src="../asset/img/sonymusic.png" alt="">
+                </div>
+                <div>
+                    <img src="../asset/img/warner.png" alt="">
+                </div>
+                <div>
+                    <img src="../asset/img/yg.png" alt="">
+                </div>
+                <div>
+                    <img src="../asset/img/jyp.png" alt="">
+                </div>
+                <div>
+                    <img src="../asset/img/belive.png" alt="">
+                </div>
             </div>
-            <div >
-                <img src="../asset/img/sonymusic.png" alt="">
+            <div class="d-flex mt-4 ">
+                <div class="d-flex me-5 pt-4"><h6>Kết nối với chúng tôi</h6></div>
+                <div class="d-flex align-items-center column-gap-3  px-4  ">
+                    <a class="ms-2" href=""><i class="fa-brands fa-facebook fa-2xl"></i></a>
+                    <a class="ms-2" href=""><i class="fa-brands fa-tiktok fa-2xl"></i></a>
+                    <a class="ms-2" href=""><i class="fa-brands fa-square-instagram fa-2xl"></i></a>
+                </div>
+                <div class="logo ">
+                    <img src="../asset/img/google.png" alt="">
+                    <img src="../asset/img/app.jpg" alt="">
+                    <img src="../asset/img/exp.png" alt="">
+                </div>
             </div>
-            <div >
-                <img src="../asset/img/warner.png" alt="">
+            <div class="d-flex">
+                <div class="nct-com">
+                    <div class="mt-5 px-4"><h2>NTC</h2></div>
+                    <div class="mb-2"><p>CORPORATION</p></div>
+                </div>
+
+
+                <div class="mt-4 pt-2">
+                    <div class="px-4"><h6>CÔNG TY CỔ PHẦN N T C</h6></div>
+                    <ul>
+                        <li>Giấy phép MXH số 499/GP-BTTTT do Bộ Thông Tin và Truyền thông cấp ngày 28/09/2015.</li>
+                        <li>Giấy Chứng nhận Đăng ký Kinh doanh số 0305535715 do Sở kế hoạch và Đầu tư thành phố Hồ Chí
+                            Minh cấp ngày 01/03/2008.
+                        </li>
+                    </ul>
+
+                </div>
+                <div class="imgg mt-5">
+                    <img src="../asset/img/bct.png" alt="">
+                    <img src="../asset/img/ca.png" alt="">
+                </div>
             </div>
-            <div >
-                <img src="../asset/img/yg.png" alt="">
-            </div>
-            <div >
-                <img src="../asset/img/jyp.png" alt="">
-            </div>
-            <div >
-                <img src="../asset/img/belive.png" alt="">
-            </div>
-        </div >
-        <div  class="d-flex mt-4 ">
-            <div class="d-flex me-5 pt-4"> <h6>Kết nối với chúng tôi</h6></div>
-            <div class="d-flex align-items-center column-gap-3  px-4  ">
-                <a class="ms-2" href=""><i class="fa-brands fa-facebook fa-2xl"></i></a>
-                <a class="ms-2" href=""><i class="fa-brands fa-tiktok fa-2xl"></i></a>
-                <a class="ms-2" href=""><i class="fa-brands fa-square-instagram fa-2xl"></i></a>
-            </div>
-            <div class="logo ">
-                <img src="../asset/img/google.png" alt="">
-                <img src="../asset/img/app.jpg" alt="">
-                <img src="../asset/img/exp.png" alt="">
-            </div>
-    </div>
-        <div class="d-flex">
-            <div class="nct-com">
-                <div class="mt-5 px-4"><h2>NTC</h2></div>
-                <div class="mb-2"><p>CORPORATION</p></div>
-            </div >
-
-        
-
-        <div class="mt-4 pt-2">
-           <div class="px-4"> <h6>CÔNG TY CỔ PHẦN N T C</h6> </div>
-            <ul>
-                <li>Giấy phép MXH số 499/GP-BTTTT do Bộ Thông Tin và Truyền thông cấp ngày 28/09/2015.</li>
-                <li>Giấy Chứng nhận Đăng ký Kinh doanh số 0305535715 do Sở kế hoạch và Đầu tư thành phố Hồ Chí Minh cấp ngày 01/03/2008.</li>
-            </ul>
-            
-        </div>
-        <div class="imgg mt-5">
-            <img src="../asset/img/bct.png" alt="">
-            <img src="../asset/img/ca.png" alt="">
-        </div>
-    </div>
-
-
-
-
-
-
-
-
 
 
     </footer>
 
-      
+
     <?php
 
 }
