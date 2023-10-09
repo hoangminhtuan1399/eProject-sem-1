37408400614bfaecc620e3bed87a81793c3fb38b
Ninh Viet Phuong <hotro.phuongsieunhan@gmail.com>
2023-10-10 00:52:44 +0700

    phuong-singerpage add singerpage

--- frontend/component/Header/HeaderComponent.php
+++ frontend/component/Header/HeaderComponent.php
@@ -2,105 +2,57 @@
 function HeaderComponent()
 {
 ?>
-    <link rel="stylesheet" href="../component/Header/HeaderComponent.css" />
-    <nav class="navbar navbar-expand-sm header__navbar mb-4">
-        <div class="container-fluid d-flex align-items-center">
-            <a class="navbar-brand" href="#" style="font-family: Arial, Helvetica, sans-serif;">
-                <img src="../img/nhaccuatui.jpeg" alt="nhaccuatui" class="rounded img-fluid" style="width: 25px; height: 25px;">
-                <strong class="header__logo-text">NCT</strong></a>
-            <ul class="navbar-nav me-auto">
-            <li class="nav-item dropdown position-static">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-            data-mdb-toggle="dropdown" aria-expanded="false">
-            Bài Hát
-          </a>
+  <link rel="stylesheet" href="../component/Header/HeaderComponent.css" />
+  <nav class="navbar navbar-expand-sm header__navbar mb-4">
+    <div class="container-fluid d-flex align-items-center">
+      <a class="navbar-brand" href="#" style="font-family: Arial, Helvetica, sans-serif;">
+        <img src="../img/nhaccuatui.jpeg" alt="nhaccuatui" class="rounded img-fluid" style="width: 25px; height: 25px;">
+        <strong class="header__logo-text">NCT</strong></a>
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item dropdown position-static">
           <div class="dropdown-menu w-50 mt-0" aria-labelledby="navbarDropdown" style="
                           border-top-left-radius: 0;
                           border-top-right-radius: 0;
                         ">
-            <div class="container-fluid ">
-              <div class="row my-3">
-                <div class="col-md-3 col-lg-3 mb-3 mb-lg-2">
-                  <div class="list-group list-group-flush">
-                    <a href="" class="list-group-item list-group-item-action"><b>Việt Nam</b></a>
-                    <a href="" class="list-group-item list-group-item-action">Nhạc trẻ</a>
-                    <a href="" class="list-group-item list-group-item-action">Trữ Tình</a>
-                    <a href="" class="list-group-item list-group-item-action">Rap Việt</a>
-                    <a href="" class="list-group-item list-group-item-action">Pop Ballad</a>
-                    <a href="" class="list-group-item list-group-item-action">Nhạc Trịnh</a>
-                  </div>
-                </div>
-                <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
-                  <div class="list-group list-group-flush">
-                    <a href="" class="list-group-item list-group-item-action"><b>Âu Mỹ</b></a>
-                    <a href="" class="list-group-item list-group-item-action">Pop</a>
-                    <a href="" class="list-group-item list-group-item-action">Rock</a>
-                    <a href="" class="list-group-item list-group-item-action">Latin</a>
-                    <a href="" class="list-group-item list-group-item-action">Indie</a>
-                    <a href="" class="list-group-item list-group-item-action">Blues/Jazz</a>
-                    <a href="" class="list-group-item list-group-item-action">Âu Mỹ Khác</a>
-                  </div>
-                </div>
-                <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
-                  <div class="list-group list-group-flush">
-                    <a href="" class="list-group-item list-group-item-action"><b>Châu Á</b></a>
-                    <a href="" class="list-group-item list-group-item-action">Nhạc Hàn</a>
-                    <a href="" class="list-group-item list-group-item-action">Nhạc Hoa</a>
-                    <a href="" class="list-group-item list-group-item-action">Nhạc Ấn Độ </a>
-                    <a href="" class="list-group-item list-group-item-action">Nhạc Campuchia</a>
-                    <a href="" class="list-group-item list-group-item-action">Nhạc Nhật </a>
-                    <a href="" class="list-group-item list-group-item-action">Nhạc Thái</a>
-                  </div>
-                </div>
-                <div class="col-md-6 col-lg-3 mb-3 mb-lg-0">
-                  <div class="list-group list-group-flush">
-                    <a href="" class="list-group-item list-group-item-action"><b>Khác</b></a>
-                    <a href="" class="list-group-item list-group-item-action">Thiếu Nhi</a>
-                    <a href="" class="list-group-item list-group-item-action">Không Lời</a>
-                    <a href="" class="list-group-item list-group-item-action">Beat</a>
-                    <a href="" class="list-group-item list-group-item-action">Thể Loại Khác</a>
-                    <a href="" class="list-group-item list-group-item-action">Tui hát</a>
-                  </div>
-                </div>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown" href="#" id="dropdownMenuButton" style="color: black;">Tuyển Tập</a>
-                    <ul class="dropdown-menu rounded-1 shadow-sm" aria-labelledy="dropdownMenuButton">
-                        <li><a class="dropdown-item" href="#">Ballad</a></li>
-                        <li><a class="dropdown-item" href="#">Rock</a></li>
-                        <li><a class="dropdown-item" href="#">Pop</a></li>
-                        <li><a class="dropdown-item" href="#">Rap</a></li>
-                        <li><a class="dropdown-item" href="#">Nhạc Trẻ</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown" href="#" id="dropdownMenuButton" style="color: black;">
-                        BXH
-                    </a>
-                    <ul class="dropdown-menu rounded-1 shadow-sm" aria-label="dropdownMenuButton">
-                        <li><a class="dropdown-item" href="#">Việt Nam</a></li>
-                        <li><a class="dropdown-item" href="#">Quốc Tế</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown" href="#" id="dropdownMenuButton" style="color: black;">
-                        Nghệ Sĩ
-                    </a>
-                    <ul class="dropdown-menu rounded-1 shadow-sm" aria-labelledby="dropdownMenuButton">
-                        <li><a class="dropdown-item" href="#">Việt Nam</a></li>
-                        <li><a class="dropdown-item" href="#">Quốc Tế</a></li>
-                    </ul>
-                </li>
-            </ul>
-            <div class="d-flex align-items-stretch column-gap-2">
-                <input class="form-control" type="text" placeholder="Tìm kiếm" aria-label="search" style="width: 360px;" id="icon-check">
-                <button type='button' class="px-3 text-decoration-none rounded-2 d-flex justify-content-center align-items-center header__login-link" data-toggle="modal" data-target="#UpWindow">Đăng nhập</button>
-                <a class="px-3 text-reset text-decoration-none rounded-2 d-flex justify-content-center align-items-center" href="#" style="color: white;">Đăng kí</a>
-            </div>
-        </div>
-        </ul>
-    </nav>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="dropdownMenuButton" style="color: black;">Tuyển Tập</a>
+          <ul class="dropdown-menu rounded-1 shadow-sm" aria-labelledy="dropdownMenuButton">
+            <li><a class="dropdown-item" href="#">Ballad</a></li>
+            <li><a class="dropdown-item" href="#">Rock</a></li>
+            <li><a class="dropdown-item" href="#">Pop</a></li>
+            <li><a class="dropdown-item" href="#">Rap</a></li>
+            <li><a class="dropdown-item" href="#">Nhạc Trẻ</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="dropdownMenuButton" style="color: black;">
+            BXH
+          </a>
+          <ul class="dropdown-menu rounded-1 shadow-sm" aria-label="dropdownMenuButton">
+            <li><a class="dropdown-item" href="#">Việt Nam</a></li>
+            <li><a class="dropdown-item" href="#">Quốc Tế</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown" href="#" id="dropdownMenuButton" style="color: black;">
+            Nghệ Sĩ
+          </a>
+          <ul class="dropdown-menu rounded-1 shadow-sm" aria-labelledby="dropdownMenuButton">
+            <li><a class="dropdown-item" href="#">Việt Nam</a></li>
+            <li><a class="dropdown-item" href="#">Quốc Tế</a></li>
+          </ul>
+        </li>
+      </ul>
+      <div class="d-flex align-items-stretch column-gap-2">
+        <input class="form-control" type="text" placeholder="Tìm kiếm" aria-label="search" style="width: 360px;" id="icon-check">
+        <button type='button' class="px-3 text-decoration-none rounded-2 d-flex justify-content-center align-items-center header__login-link" data-toggle="modal" data-target="#UpWindow">Đăng nhập</button>
+        <a class="px-3 text-reset text-decoration-none rounded-2 d-flex justify-content-center align-items-center" href="#" style="color: white;">Đăng kí</a>
+      </div>
+    </div>
+    </ul>
+  </nav>
 <?php
 
 }
 
-    ?>
+?>
--- frontend/page/singerpage.php
+++ frontend/page/singerpage.php
@@ -1,5 +1,7 @@
 <?php
 include_once "../component/Header/HeaderComponent.php";
+include_once "../component/FeauturedSongSingers/FeauturedSongSingers.php";
+include_once __DIR__ . "/../../backend/api/Album/AlbumView.class.php";
 include_once __DIR__ . "/../../backend/api/Song/SongView.class.php";
 include_once __DIR__ . "/../../backend/api/Singer/SingerView.class.php";
 $SingerView = new SingerView();
@@ -9,10 +11,12 @@ $singers = $singerData[0];
 $singerView = new SingerView();
 $PictureSingers = $singerView->showAllSinger('', '', 8);
 $SongView = new SongView();
-$SongBySinger = $SongView->showSongBySingerId($singerId, 6);
-
-
+$SongBySinger = $SongView->showSongBySingerId($singerId, 10);
+$AlbumView = new AlbumView();
+$AlbumViewSingerId = $AlbumView->showAlbumBySingerId($singerId);
+$AlbumViewSingers = $AlbumViewSingerId[0];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,6 +24,7 @@ $SongBySinger = $SongView->showSongBySingerId($singerId, 6);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/9a6d25af5b.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="singerpage.css">
     <title><?php echo $singers['name'] ?></title>
 </head>
@@ -34,39 +39,38 @@ $SongBySinger = $SongView->showSongBySingerId($singerId, 6);
         <div class="row">
             <div class="col-8">
                 <h3 style="color: #2daaed">Tiểu sử ></h3>
-                <div class="d-flex">
-                    <img src="<?php echo $singers['image'] ?>" alt="<?php echo $singers['name'] ?>" class="rounded-circle img-fluid me-3 " style="width: 150px; height: 150px; flex-shrink:0;">
-                    <p style="max-width:400px"><?php echo $singers['description'] ?></p>
-                </div>
-                <div class="mt-3">
-                    <div class="container">
-                        <h2><?php echo $singers['name'] ?></h2>
-                        <h3 style="color: #2daaed">Album <?php echo $singers['name'] ?> ></h3>
-                        <div class="d-flex flex-wrap gap-2">
-                            <?php
-                            foreach ($SongBySinger as $songSortedByViews) {
-                            ?>
-                                <div class="col-3 ">
-                                    <div class="col SongBySinger__item-wrapper">
-                                        <div class="d-flex align-items-center justify-content-center">
-                                            <a href="#?id=<?php echo $songSortedByViews['singer_id'] ?>" class="text-decoration-none featured-song__singer-name">
-                                                <img class="object-fit-cover SongBySinger-img" src="<?php echo $songSortedByViews['image'] ?>">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            <?php
-                            } ?>
+                <div class="container">
+                    <div class="d-flex flex-column">
+                        <div class="d-flex">
+                            <div class="d-flex flex-column align-items-center">
+                                <img src="<?php echo $singers['image'] ?>" alt="<?php echo $singers['name'] ?>" class="rounded-circle img-fluid me-3 " style="width: 150px; height: 150px; flex-shrink:0;">
+                                <h4><?php echo $singers['name'] ?></h4>
+                                <span>Sinh Nhật: <?php echo $singers['birth_year'] ?></span>
+                                <span>Giới tính: <?php echo $singers['gender'] ?> </span>
+                                <span>Quốc Gia: <?php echo $singers['nationality'] ?></span>
+                            </div>
+                            <p style="max-width:400px"><?php echo $singers['description'] ?></p>
                         </div>
                     </div>
+                </div>
+                <div class="mt-4">
+                    <div class="container">
+                        <h3 style="color: #2daaed">Album <?php echo $singers['name'] ?> ></h3>
+                        <img class="album-item-wrapper m-1" src="<?php echo $AlbumViewSingers['image'] ?>" style="width: 150px; height: 150px; flex-shrink:0;">
+                        <h5><?php echo $AlbumViewSingers['name'] ?></h5>
+                        <div class="mt-3">
+                        <h3 style="color: #2daaed">Bài Hát <?php echo $singers['name'] ?></h3>
+                        </div>
+                    </div>
+                    <?php FeaturedSongSingers($SongBySinger); ?>
                 </div>
 
             </div>
             <div class="col-4">
                 <h3 style="color: #2daaed">Ca Sĩ | Nghệ Sĩ></h3>
-                <div class="d-flex flex-wrap">
+                <div class="d-flex flex-wrap gap-2">
                     <?php foreach ($PictureSingers as $PictureSinger) {
-                    ?><div class="w-50">
+                    ?><div class="singerpage-img-container">
                             <a href="singerpage.php?id=<?php echo $PictureSinger['singer_id'] ?>" class="text-decoration-none featured-song__singer-name">
                                 <img class="img-fluid singerpage-img" src="<?php echo $PictureSinger['image'] ?>">
                             </a>
