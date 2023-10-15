b9e3da3dbb21c796196f8462f34e14e1aa69a8e2
Nguyen Truong Giang <giang.nt.2309@aptechlearning.edu.vn>
2023-10-16 01:10:46 +0700

    giang-pagination - add pagination

--- frontend/page/album.php
+++ frontend/page/album.php
@@ -10,12 +10,17 @@ include_once "../component/FeaturedCategory/FeaturedCategory.php";
 include_once "../component/NextSong/NextSong.php";
 $id = $_GET['id'];
 $SongView = new SongView();
-$SongByAlbumId = $SongView->showSongByAlbumId($id);
-$FirstSong = $SongByAlbumId[0];
-$songByIdAlbum = $SongView->showSongByAlbumId($id, $limit = 10, $offset = 0);
 $AlbumView = new AlbumView();
-$albumSortedByDate = $AlbumView->showAllAlbum('released_date', '');
+$albumSortedByDate = $AlbumView->showAllAlbum('released_date', '', $limit = 8);
 $currentAlbum = $AlbumView->showAlbumById($id)[0];
+
+$limit = 8;
+$page = $_GET['page'] ?? 1;
+$offset = $limit * ($page - 1);
+$songCount = $SongView->showSongCountByAlbumId($id);
+$pageCount = ceil($songCount / $limit);
+$songByIdAlbum = $SongView->showSongByAlbumId($id, $limit, $offset);
+$FirstSong = $songByIdAlbum[0];
 ?>
 
 <!DOCTYPE html>
@@ -26,80 +31,112 @@ $currentAlbum = $AlbumView->showAlbumById($id)[0];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="../asset/css/index.css">
-    <title><?php echo $currentAlbum['name'] ?></title>
+    <title>
+        <?php echo $currentAlbum['name'] ?>
+    </title>
 </head>
 
 <body>
-<header>
-    <?php
-    HeaderComponent();
-    ?>
-</header>
-<main>
-    <div class="container">
-        <div class="row">
-            <div class="col-8 ">
-                <h4 class="mb-4">
-                    <strong>
-                        <?php echo $FirstSong['name'] ?> -
-                        <?php echo $FirstSong['singer_name'] ?>
-                    </strong>
-                </h4>
-                <div>
-                    <audio class="col-md-10 mb-2" controls controlslist="nodownload" autoplay src=" <?php echo $FirstSong['file_name'] ?>"></audio>
-                </div>
-                <div class="accordion" id="accordionExample">
-                    <div class=" accordion-item">
-                        <h5 accordion-header>
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse"
+    <header>
+        <?php
+        HeaderComponent();
+        ?>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-8 ">
+                    <h4 class="mb-4">
+                        <strong>
+                            <?php echo $FirstSong['name'] ?> -
+                            <?php echo $FirstSong['singer_name'] ?>
+                        </strong>
+                    </h4>
+                    <div>
+                        <audio class="col-md-10 mb-2" controls controlslist="nodownload" autoplay
+                            src=" <?php echo $FirstSong['file_name'] ?>"></audio>
+                    </div>
+                    <div class="accordion" id="accordionExample">
+                        <div class=" accordion-item">
+                            <h5 accordion-header>
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                <b> Lời Bài hát :
-                                    <?php echo $FirstSong['name'] ?>
-                                </b>
-                            </button>
-                        </h5>
-                        <div id="collapseOne" class="accordion-collapse collapse show"
-                             data-bs-parent="#accordionExample">
-                            <div class="accordion-body fs-7">
-                                <?php echo $FirstSong['lyrics'] ?>
+                                    <b> Lời Bài hát :
+                                        <?php echo $FirstSong['name'] ?>
+                                    </b>
+                                </button>
+                            </h5>
+                            <div id="collapseOne" class="accordion-collapse collapse show"
+                                data-bs-parent="#accordionExample">
+                                <div class="accordion-body fs-7">
+                                    <?php echo $FirstSong['lyrics'] ?>
+                                </div>
                             </div>
+                            <p class="px-3">Lời đăng bởi :
+                                <a href="https://www.facebook.com/master.giang.9"> Nguyễn Trường Giang</a>
+                            </p>
                         </div>
-                        <p class="px-3">Lời đăng bởi :
-                            <a href="https://www.facebook.com/master.giang.9"> Nguyễn Trường Giang</a>
-                        </p>
+                    </div>
+                    <div class="mt-4">
+                        <?php FeaturedAlbum($albumSortedByDate, 'Album khác') ?>
                     </div>
                 </div>
-                <div class="mt-4">
-                    <?php FeaturedAlbum($albumSortedByDate, 'Album khác') ?>
-                </div>
-            </div>
-            <div class="col-4">
-                <div class="ms-4">
-                    <h4 class="fw-bold">NGHE TIẾP</h4>
-                    <?php
-                    NextSong($songByIdAlbum);
-                    ?>
-                </div>
-                <div class="mt-5">
-                    <?php
-                    FearuredCategory();
-                    ?>
-                </div>
-            </div>
-        </div>
-</main>
-<?php
-?>
+                <div class="col-4">
+                    <div class="ms-4">
+                        <h4 class="fw-bold">NGHE TIẾP</h4>
+                        <?php
+                        NextSong($songByIdAlbum);
+                        ?>
+                        <nav class="mt-4 d-flex justify-content-center">
+                            <ul class="pagination">
+                                <li class="page-item <?php echo ($page <= 1 ? "disabled" : "") ?>">
+                                    <a class="page-link"
+                                        href="album.php?id=<?php echo $id ?>&page=<?php echo $page - 1 ?>"
+                                        aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                                <?php
+                                for ($i = 1; $i <= $pageCount; $i++) {
+                                    ?>
+                                    <li class="page-item <?php echo ($i == $page ? "active" : "") ?>"> <a class="page-link"
+                                            href="album.php?id=<?php echo $id ?>&page=<?php echo $i ?>">
+                                            <?php echo $i ?>
+                                        </a></li>
+                                    <?php
+                                }
+                                ?>
+                                <li class="page-item  <?php echo ($page >= $pageCount ? "disabled" : "") ?>">
+                                    <a class="page-link "
+                                        href="album.php?id=<?php echo $id ?>&page=<?php echo $page + 1 ?>"
+                                        aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
 
-<footer>
+                    </div>
+                    <div class="mt-5">
+                        <?php
+                        FearuredCategory();
+                        ?>
+                    </div>
+                </div>
+            </div>
+    </main>
     <?php
-    FooterComponent();
     ?>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+
+    <footer>
+        <?php
+        FooterComponent();
+        ?>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
-<script src="https://kit.fontawesome.com/9a6d25af5b.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/9a6d25af5b.js" crossorigin="anonymous"></script>
 
 </body>
 
--- frontend/page/all-singer.php
+++ frontend/page/all-singer.php
@@ -6,13 +6,18 @@ include_once "../component/Footer/FooterComponent.php";
 include_once "../component/FeaturedSong/FeaturedSong.php";
 include_once __DIR__ . "/../component/FeaturedSinger/FeaturedSinger.php";
 include_once "../../backend/api/Song/SongView.class.php";
+
 $mode = $_GET['mode'];
 $singerView = new singerView();
-$singer = $singerView->showLocalOrInternationalSinger($mode);
-// print_r($singer);
 
 $SongView = new SongView();
 $songSortedByView = $SongView->showAllSong('views', 'desc', 10);
+$limit = 8;
+$page = $_GET['page'] ?? 1;
+$offset = $limit * ($page - 1);
+$singerCount = $singerView->showLocalOrInternationalSingerCount($mode);
+$pageCount = ceil($singerCount / $limit);
+$singer = $singerView->showLocalOrInternationalSinger($mode, $limit, $offset);
 ?>
 
 <!DOCTYPE html>
@@ -28,34 +33,61 @@ $songSortedByView = $SongView->showAllSong('views', 'desc', 10);
 </head>
 
 <body>
-<header>
-    <?php
-    HeaderComponent();
-    ?>
-</header>
-<main>
-    <div class="container">
-        <div class="row">
-            <div class="col-8 flex-gap ">
-                <div class="img">
+    <header>
+        <?php
+        HeaderComponent();
+        ?>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-8 flex-gap ">
+                    <div class="img">
+                        <?php
+                        FeaturedSinger($singer, 'Nghệ Sĩ ' . ($mode === 'local' ? 'Việt Nam' : 'Quốc Tế'));
+                        ?>
+                        <nav class="mt-4 d-flex justify-content-center">
+                            <ul class="pagination">
+                                <li class="page-item <?php echo ($page <= 1 ? "disabled" : "") ?>">
+                                    <a class="page-link"
+                                        href="all-singer.php?mode=<?php echo $mode ?>&page=<?php echo $page - 1 ?>"
+                                        aria-label="Previous">
+                                        <span aria-hidden="true">&laquo;</span>
+                                    </a>
+                                </li>
+                                <?php 
+                                for ($i = 1 ; $i <= $pageCount ; $i++){           
+                                    ?>
+                                    <li class="page-item <?php echo ($i == $page ? "active" : "") ?>"> <a class="page-link"
+                                            href="all-singer.php?mode=<?php echo $mode ?>&page=<?php echo $i ?>">
+                                            <?php echo $i ?>
+                                        </a></li>
+                                <?php
+                                }
+                                ?>
+
+                                <li class="page-item <?php echo ($page >= $pageCount ? "disabled" : "") ?>">
+                                    <a class="page-link" href="all-singer.php?mode=<?php echo $mode ?>&page=<?php echo $page + 1 ?>" aria-label="Next">
+                                        <span aria-hidden="true">&raquo;</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-4">
                     <?php
-                    FeaturedSinger($singer, 'Nghệ Sĩ ' . ($mode === 'local' ? 'Việt Nam' : 'Quốc Tế'));
+                    FeaturedSong($songSortedByView);
                     ?>
                 </div>
             </div>
-            <div class="col-4">
-                <?php
-                FeaturedSong($songSortedByView);
-                ?>
-            </div>
         </div>
-    </div>
-</main>
-<footer>
-    <?php
-    FooterComponent();
-    ?>
-</footer>
+    </main>
+    <footer>
+        <?php
+        FooterComponent();
+        ?>
+    </footer>
 
 </body>
 
