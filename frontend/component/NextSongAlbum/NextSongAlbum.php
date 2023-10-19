a2f0ba45c19d58c4426b326260aab7ebb29eb0d1
Nguyen Truong Giang <giang.nt.2309@aptechlearning.edu.vn>
2023-10-19 21:23:03 +0700

    giang-fix_album

---
+++ frontend/component/NextSongAlbum/NextSongAlbum.php
@@ -0,0 +1,23 @@
+<?php
+function NextSongAlbum($songs, $albumId, $page)
+{
+    ?>
+    <link rel="stylesheet" href="../component/NextSong/NextSong.css" />
+    <?php
+    foreach ($songs as $song) {
+        ?>
+        <div class="d-flex align-items-center py-2 border-bottom">
+            <div class="d-flex flex-column">
+                <a href="album.php?id=<?php echo $albumId ?>&songId=<?php echo $song['song_id'] ?>&page=<?php echo $page ?>"
+                    class="text-decoration-none link-body-emphasis next-song__song-name">
+                    <?php echo $song['name'] ?>
+                </a>
+                <a href="singerpage.php?id=<?php echo $song['singer_id'] ?>"
+                    class="text-decoration-none emphasis next-song__singer-name">
+                    <?php echo $song['singer_name'] ?>
+                </a>
+            </div>
+        </div>
+        <?php
+    }
+}
